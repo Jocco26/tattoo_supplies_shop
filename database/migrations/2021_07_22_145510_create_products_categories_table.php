23653f04d626e05951db7bb71a641bdc119e2fb2
unknown <joks026@gmail.com>
2021-07-22 23:50:54 +0800

    oh my

---
+++ database/migrations/2021_07_22_145510_create_products_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('supply_category_id');
+            $table->timestamps();
+
+            $table->foreign('supply_category_id')->references('id')->on('supplies_categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products_categories');
+    }
+}
---
+++ resources/js/components/TopBar.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#"><img :src="'../storage/images/newlogo.png'"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" 
+                data-target="#navbarResponsive">
+                <span class="navbar-toggler-icon"></span>
+    </button>
+  
+  <div class="collapse navbar-collapse" id="navbarResponsive">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item dropdown" v-for="category in categories" v-bind:key="category.id">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          {{category.name}}
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
+</div>
+</template>
+<script>
+import axios from 'axios';
+
+  export default{
+    data(){
+      return{
+        categories:[]
+      }
+    },
+    created(){
+        this.SuppliesCategory();
+    },
+    methods:{
+      SuppliesCategory(){
+        axios
+        .get('api/supplies')
+        .then((response) => {
+          console.log(response.data)
+          this.categories = response.data
+        })
+        .catch((erroe) => {
+          console.log(error)
+          this.errorMsg = 'Error retrieving data'
+        })
+      }
+    }
+  }
+</script>
